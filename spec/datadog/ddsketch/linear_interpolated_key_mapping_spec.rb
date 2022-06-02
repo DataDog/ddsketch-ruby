0134fb5917bceaeecbfcdc364019d69e60484e65
Tony Hsu <tonyc.t.hsu@gmail.com>
2022-06-02 11:47:11 +0200

    Skip two mapping spec for JRuby

--- spec/datadog/ddsketch/linear_interpolated_key_mapping_spec.rb
+++ spec/datadog/ddsketch/linear_interpolated_key_mapping_spec.rb
@@ -1,4 +1,6 @@
 describe Datadog::DDSketch::LinearlyInterpolatedKeyMapping do
+  before { skip 'Skipping `Math.ldexp` is inconsistent between JRuby & MRI' if RUBY_PLATFORM == 'java'  }
+
   include_context 'mapping tests' do
     let(:mapping) { described_class.new(relative_accuracy: relative_accuracy, offset: offset) }
   end
