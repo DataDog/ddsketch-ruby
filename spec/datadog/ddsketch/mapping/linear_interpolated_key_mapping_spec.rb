34a937aad3bec3c194375aceffb973dab8e05ae5
Tony Hsu <tonyc.t.hsu@gmail.com>
2022-06-29 16:43:02 +0200

    Fix spec scoping

--- spec/datadog/ddsketch/mapping/linear_interpolated_key_mapping_spec.rb
+++ spec/datadog/ddsketch/mapping/linear_interpolated_key_mapping_spec.rb
@@ -1,11 +1,13 @@
 describe Datadog::DDSketch::Mapping::LinearlyInterpolatedKeyMapping do
-  before { skip 'Skipping `Math.ldexp` is inconsistent between JRuby & MRI' if RUBY_PLATFORM == 'java' }
-
   it_behaves_like 'mapping protobuf', :LINEAR do
     subject { described_class.new(relative_accuracy: 0.01) }
   end
 
-  include_context 'mapping tests' do
-    let(:mapping) { described_class.new(relative_accuracy: relative_accuracy, offset: offset) }
+  describe do
+    before { skip 'Skipping `Math.ldexp` is inconsistent between JRuby & MRI' if RUBY_PLATFORM == 'java' }
+
+    include_context 'mapping tests' do
+      let(:mapping) { described_class.new(relative_accuracy: relative_accuracy, offset: offset) }
+    end
   end
 end
